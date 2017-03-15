6f56322f2355e68c84615fd35a75fbb41d53f85b
Monica Jones <monicaj314@yahoo.com>
2017-03-14 23:00:25 -0500

    deleted duplicates

--- group_project/group_project/public/assets/js/firebase-config.js
+++
@@ -1,9 +0,0 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDJKSN5Rcxv658qbljGCRV8Nhk9TXgGqPI",
-    authDomain: "wine-buddy-abd6c.firebaseapp.com",
-    databaseURL: "https://wine-buddy-abd6c.firebaseio.com",
-    storageBucket: "wine-buddy-abd6c.appspot.com",
-    messagingSenderId: "617681829711"
-  };
-  firebase.initializeApp(config);
